017c4a63bf217ca10f93c1c003f3b13f9a84a133
Jon Jaroker <gitlab@jaroker.com>
2022-03-01 09:48:33 -0500

    debugging duplicate lines

--- manifests/install.pp
+++ manifests/install.pp
@@ -64,7 +64,7 @@ class pihole::install {
     file_line { $k:
       path               => $setup_vars_conf,
       line               => "${k}=${v}",
-      match              => "^${k}=",
+      match              => "^${k}",
       multiple           => false, # Should only be one instance of a variable
       replace            => true,
       append_on_no_match => true,
